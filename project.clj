1cafa2bd46d45c274c7fa4e4a1ea773aa01b0a96
Martin Henke <martin.henke@de.ibm.com>
2026-03-30 14:49:50 +0200

    remove outdated files

--- project.clj
+++
@@ -1,4 +0,0 @@
-(defproject antopt "1.0.0-SNAPSHOT"
-  :description "Traveling salesman using ant colony optimization"
-  :dependencies [[org.clojure/clojure "1.8.0"] [seesaw "1.4.5"]]
-  :main antopt.antoptui)
